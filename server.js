247388dfdd28e3e5c9f50fbc7d858fbd9403fa6c
MaxZero-Twinsynergy <maxzerozity@gmail.com>
2021-01-30 01:48:48 +0700

    demo chat

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {
+    cors: {
+      origin: "http://localhost:4200",
+      methods: ["GET", "POST"]
+    }
+  });
+var cors = require('cors')
 
-
-
-app.get('/', (req, res) => res.send('hello!'));
+app.use(cors())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    next();
+  });
+app.get('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.send('hello!')
+});
 
 
 io.on('connection',(socket) =>{
     console.log('a user connected')
+    socket.on('message',(msg)=>{
+        console.log(msg);
+        socket.broadcast.emit('message-broadcast',msg);
+    });
 });
 
 
