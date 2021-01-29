cb06d0b41f229c09105c1ce30b95eb5623105bf2
MaxZero-Twinsynergy <maxzerozity@gmail.com>
2021-01-29 16:45:13 +0700

    Shopping

--- newApp/src/app/shopping-list/shopping-list.component.ts
+++ newApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -15,5 +16,9 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onIngredientAdded(ingredient: Ingredient){
 
+    this.ingredients.push(ingredient);
+
+  }
 }
