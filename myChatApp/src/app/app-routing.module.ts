e5e283a59c6ae8c74c5abaa27091723e3042729d
MaxZero-Twinsynergy <maxzerozity@gmail.com>
2021-01-30 13:26:06 +0700

    สร้างหน้า login

--- myChatApp/src/app/app-routing.module.ts
+++ myChatApp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { RegisterComponent } from './component/register/register.component';
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myChatApp/src/app/app.module.ts
+++ myChatApp/src/app/app.module.ts
@@ -5,11 +5,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { RegisterComponent } from './component/register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatInboxComponent
+    ChatInboxComponent,
+    LoginComponent,
+    DashboardComponent,
+    NavbarComponent,
+    ProfileComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
