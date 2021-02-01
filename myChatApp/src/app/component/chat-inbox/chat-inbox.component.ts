5c12b5f8d6ebd13ab98b5cb70b600d2056e2245c
MaxZero-Twinsynergy <maxzerozity@gmail.com>
2021-02-01 23:47:59 +0700

    นำข้อความเก่ามา ใส่

--- myChatApp/src/app/component/chat-inbox/chat-inbox.component.ts
+++ myChatApp/src/app/component/chat-inbox/chat-inbox.component.ts
@@ -22,11 +22,30 @@ export class ChatInboxComponent implements OnInit {
   ngOnInit(): void {
     this.setupSocketConnection();
   }
+
+
+
   setupSocketConnection(){
     this.roomId = this.activatedRoute.snapshot.paramMap.get("id");
     // console.log('roomId ==',this.roomId);
     this.socket = io(SOCKET_ENDPOINT,{ query: "roomId="+this.roomId });
     // console.log(this.socket.)
+
+    this.restApi.getMessageByRoom(this.roomId).subscribe((data: {}) => {
+        console.log('data ==',data)
+        for (var item of data['data']) {
+          console.log(item['text']);
+          const element = document.createElement('li');
+          element.innerHTML = item['text'] ? item['text']:'';
+          element.style.background = 'white';
+          element.style.padding =  '15px 30px';
+          element.style.margin = '10px';
+          element.style.textAlign = 'right';
+          document.getElementById('message-list').appendChild(element);
+        }
+
+    })
+
     this.socket.on('message-broadcast', (data: string) => {
       console.log('message-broadcast',data)
     if (data) {
